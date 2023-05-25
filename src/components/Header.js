abf316839da08b0b933dbda7e16320b1c13acd91
Anton <borzenkov0anton@gmail.com>
2023-05-25 23:31:32 +0200

    header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,20 @@
 import React from 'react'
 import './Header.css';
-import ''
+import logo from '../image/logo192.png';
 
 export default function Header() {
   return (
     <div className='App-header'>
         <span className='text'>
-            NETFLIX
+            <img src={logo} alt = 'netflix' width="300" height="40"></img>
         </span>
-        <span className='tv-shows'>UNLIMITED TV SHOWS & MOVIES</span>
+        <span className='tv-shows'> 
+            <p>
+                UNLIMITED TV SHOWS & MOVIES
+            </p>
+        </span>
+        <button className='join'>Join now</button>
+        <button className='sign-in'>Sign-out</button>
     </div>
   )
 }
