e9acd814f433808b187a0234975c0c3154647190
Anton <borzenkov0anton@gmail.com>
2023-05-25 22:55:40 +0200

    bckgrnd setuped

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Header.css';
+
+export default function Header() {
+  return (
+    <div className='App-header'>
+    <span className='text'>
+        NETFLIX
+    </span>
+    </div>
+  )
+}
