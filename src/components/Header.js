b7a539a1fc9887bfed007d5d4ea7cecc6b930088
Anton <borzenkov0anton@gmail.com>
2023-05-25 23:03:08 +0200

    logo iss

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import './Header.css';
+import ''
 
 export default function Header() {
   return (
     <div className='App-header'>
-    <span className='text'>
-        NETFLIX
-    </span>
+        <span className='text'>
+            NETFLIX
+        </span>
+        <span className='tv-shows'>UNLIMITED TV SHOWS & MOVIES</span>
     </div>
   )
 }
